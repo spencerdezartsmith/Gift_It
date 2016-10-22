6db2cf518bdd61f7b2742dcc427c0cb6c8e741f5
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-10-22 14:00:51 -0700

    Add History component boiler plate

---
+++ src/components/History.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { View, Text } from 'react-native';
+
+class History extends Component {
+  state = { gifts: [] }
+  
+  render() {
+    return (
+      <View>
+        <Text>
+          Hello
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default History;
