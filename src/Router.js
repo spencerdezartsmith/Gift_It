bf308575918c6617c6118bc0a8f957c18486b285
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-10-22 12:49:22 -0700

    Build Router component with inital routes

---
+++ src/Router.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Scene, Router, Actions } from 'react-native-router-flux';
+import Root from './components/Root';
+
+const RouterComponent = () => {
+  return (
+    <Router
+      sceneStyle={{ paddingTop: 60 }}
+      navigationBarStyle={{ backgroundColor: '#8EFAB4' }}
+    >
+      <Scene
+        key='root'
+        component={Root}
+        rightTitle='Home'
+        onRight={() => Actions.home()}
+        initial
+       />
+       <Scene key='home' component={Graph} />
+       <Scene
+        key='history'
+        component={History}
+        title='History' />
+    </Router>
+  )
+}
