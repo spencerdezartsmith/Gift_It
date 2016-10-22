24631b24118d18a55f573e382e8cd0030c4f93b8
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-10-22 12:48:47 -0700

    Add the Root component

---
+++ src/components/Root.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Image, View } from 'react-native';
+import { Container } from './common';
+
+class Root extends Component {
+  render() {
+    return (
+      <View style={styles.contianer}>
+        <Container>
+          <Image
+            style={{ marginTop: -40 }}
+            source={require('../Resources/GiftIt_Logo_Green.png')}
+            resizeMode='contain'
+          />
+          <Image
+            style={styles.imageStyle}
+            source={require('../Resources/gift-it-blue.gif')}
+            resizeMode='contain'
+          />
+        </Container>
+      </View>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    paddingTop: 50
+  },
+  cardStyle: {
+    shadowColor: '#000000',
+    borderColor: 'white'
+  },
+  imageStyle: {
+    flex: 1,
+    width: 350,
+    height: 300,
+    resizeMode: 'contain',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+};
+
+export default Root;
