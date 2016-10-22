3f9b167ebd17ba6db256b614db4eb3e1277c882a
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-10-22 13:23:35 -0700

    Remove unnecessay imports

--- src/GiftIt.js
+++ src/GiftIt.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import Root from './components/Root';
+import { View } from 'react-native';
+import Router from './Router';
 
 class GiftIt extends Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <Text>Hello Worl</Text>
+        <Router />
       </View>
     );
   }
