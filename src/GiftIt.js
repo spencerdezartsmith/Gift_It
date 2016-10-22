c3b896c8c19c3bb0d626d02ec65a18070a08a569
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-10-22 12:01:47 -0700

    Create project. Build alternate app component. Set up registry

---
+++ src/GiftIt.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class GiftIt extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Text>Hello Worl</Text>
+      </View>
+    );
+  }
+}
+
+export default GiftIt;
