6ae75c52b06812d0b3f413c17bf5bef6fa9c70cd
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-10-22 16:07:12 -0700

    Finish the styling for the History page

--- src/components/History.js
+++ src/components/History.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { View, Text } from 'react-native';
+import ListItem from './ListItem';
 
 class History extends Component {
-  state = { gifts: [] }
-  
+  state = { gifts: [] };
+
+  componentWillMount() {
+    axios.get('http://localhost:3000/users/1/gifts')
+    .then(response => this.setState({ gifts: response.data }));
+  }
+
+  renderGifts() {
+    return this.state.gifts.map(gift =>
+      <ListItem key={gift.id} gift={gift} />
+    );
+  }
+
   render() {
     return (
       <View>
-        <Text>
-          Hello
-        </Text>
+        {this.renderGifts()}
       </View>
     );
   }
